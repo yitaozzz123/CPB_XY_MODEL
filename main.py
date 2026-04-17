f128f81961a8ae7db83378fddd7ba36af05825e6
Mario Maalini <magalinimario@gmail.com>
2026-04-17 12:16:34 +0200

    class struct

---
+++ main.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib as plt
+
+class XY_Monte_Carlo:
+    def __init__(self, temp, n_particles):
+        self.temp=temp
+        self.n_particles=n_particles
+
+    def shape():
+        return 0
+
+    def random_state():
+        return 0
+
+    def state_all_up():
+        return 0
+    
+    def transition():
+        pass
+
+    def hamiltonian():
+        return 0
+    
+    def current_state():
+        return 0
+    
+
