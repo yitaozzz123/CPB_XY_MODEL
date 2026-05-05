860b1081b45d63a7c3fe20b594f00a2ddcc9e788
Mario Maalini <magalinimario@gmail.com>
2026-05-05 19:53:34 +0200

    renaming, everything seems ready

---
+++ main_simulations.py
@@ -0,0 +1,10 @@
+from experiments import run_temperature_scan, run_external_field_scan
+
+
+def main():
+    run_temperature_scan()
+    run_external_field_scan()
+
+
+if __name__ == "__main__":
+    main()
